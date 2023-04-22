0f65d0312d1654b3b9713dc19bfd8974b5f1df3d
saurabh kumar <86232271+sa4rabh@users.noreply.github.com>
2023-04-23 01:27:55 +0530

    Create WrappedListComponent.js

---
+++ WrappedListComponent.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import SingleListItem from './SingleListItem';
+
+const List = ({ items }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  useEffect(() => {
+    setSelectedIndex(null);
+  }, [items]);
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {items.map((item, index) => (
+        <SingleListItem
+          key={index}
+          onClickHandler={handleClick}
+          text={item.text}
+          index={index}
+          isSelected={index === selectedIndex}
+        />
+      ))}
+    </ul>
+  );
+};
+
+List.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default List;
